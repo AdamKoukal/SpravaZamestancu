664b2435569e8791bb74d4166358723ee54445af
AdamKoukal <adam.koukal1@gmail.com>
2026-06-10 11:23:30 +0200

    Light interests security

--- models/addInterest.ts
+++ models/addInterest.ts
@@ -1,14 +1,26 @@
 "use server"
 import { Interests } from "@/generated/prisma/client";
 import prisma from "./prisma";
+import { auth } from "@/auth";
+import getUserById from "./getUserById";
 
 export default async function addInterest(interest:Interests){
+    const session:any  = await auth();
 
-    let a={user_id:interest.user_id,title:interest.title,description:interest.description}
-    const addedInterest= prisma.interests.create(
+    let userPosition="";
+
+    if(session){
+        userPosition=""+(await getUserById(session?.user?.id))?.position;
+    }
+
+    if(userPosition=="HR"||session?.user.id==interest.user_id)
+    {
+        let a={user_id:interest.user_id,title:interest.title,description:interest.description}
+        const addedInterest= prisma.interests.create(
         {
             data:a,
         }
-    )
-    return addedInterest;
+        )
+        return addedInterest;
+    }   
 }
--- models/removeInterestById.ts
+++ models/removeInterestById.ts
@@ -1,12 +1,29 @@
 "use server"
 
+import { auth } from "@/auth";
 import prisma from "./prisma"
+import getUserById from "./getUserById";
 
 export default async function removeInterestById(removeId:string){
-    console.log("jo");
+
+    const session:any  = await auth();
+    
+    let userPosition="";
+    
+    if(session)
+    {
+        userPosition=""+(await getUserById(session?.user?.id))?.position;
+    }
+    let profileUserId=(await prisma.interests.findUnique({where: 
+        {
+            id: removeId,
+        },}))?.user_id;
+    if(userPosition=="HR"||session?.user.id==profileUserId)
+    {
     await prisma.interests.delete({
         where:{
             id:removeId
         }
     })
+    }
 }
